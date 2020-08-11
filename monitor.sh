7573cd1a056262cf5530889e2a0c18731afe2747
Djé <4802444+jeromeminardi@users.noreply.github.com>
2020-08-11 15:58:21 +0200

    Update monitor.sh
    Added retry x5 to avoid a connexion down notification just on one issue

--- monitor.sh
+++ monitor.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-# Author: Jérôme Minardi
+#
 # version: 1.0
-# date: 10-08-2020
-# Monitor on en0 (change line 24 if needed to monitor on another device)
+# on en0
+
+
+function testConnectivity {
+  n=0
+  while :
+  do
+    nc -z -w 5 -G 1 -L 5 8.8.8.8 53 >/dev/null 2>&1
+    [[ $? = 0 ]] && break || ((n++))
+    (( n >= 5 )) && break
+  done
+}
+
 
 # Program start
-echo "`date "+%Y-%m-%d %H:%M:%S"` - MONITORING START" | tee -a internet.log
+echo "`date "+%Y-%m-%d %H:%M:%S"` - MONITORING START" | tee -a log.csv
 
 # variable initialization
-nc -z -w 2 -G 1 -L 5 8.8.8.8 53 >/dev/null 2>&1
+testConnectivity
 previous=$?
 TIMESTAMP=`date +%s`
 
 while [ 1 ]
   do
-   nc -z -w 2 -G 1 -L 5 8.8.8.8 53 >/dev/null 2>&1
+    testConnectivity
     online=$?
 
     if [ $online -eq $previous ]; then
@@ -26,11 +37,11 @@ while [ 1 ]
       if [ $STATUS = "active" ]; then
         if [ $online -eq 0 ]; then
           # connexion is now available
-          echo "`date "+%Y-%m-%d %H:%M:%S"` - DOWN DURING [$(($TIME-$TIMESTAMP))]" | tee -a internet.log
+          echo "`date "+%Y-%m-%d %H:%M:%S"` - DOWN DURING [$(($TIME-$TIMESTAMP))]" | tee -a log.csv
           say "La connexion internet a été indisponible pendant $(($TIME-$TIMESTAMP)) secondes"
         else
           # connexion is not available
-          echo "`date "+%Y-%m-%d %H:%M:%S"` - UP DURING [$(($TIME-$TIMESTAMP))]" | tee -a internet.log
+          echo "`date "+%Y-%m-%d %H:%M:%S"` - UP DURING [$(($TIME-$TIMESTAMP))]" | tee -a log.csv
           say "Attention : La connexion internet vient d'être perdue"
         fi
       fi
