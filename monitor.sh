be1b44943caf030cefe6a4d984778563752234b3
Djé <4802444+jeromeminardi@users.noreply.github.com>
2020-08-13 09:56:45 +0200

    Added formatting in output
    Make duration time readable and compatible with more than 24 hours

--- monitor.sh
+++ monitor.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 # author : jérôme Minardi
-# version: 1.0
+# version: 1.1
 #
 # Unix (Mac Os) script to monitor internet connection status and log Up & Donw status with the duration
 # Listen on en0
+
+function seconds2time {
+   local T=$1
+   local D=$((T/60/60/24))
+   local H=$((T/60/60%24))
+   local M=$((T/60%60))
+   local S=$((T%60))
+
+   if [[ ${D} != 0 ]]
+   then
+      printf '%d days %02d:%02d:%02d' $D $H $M $S
+   else
+      printf '%02d:%02d:%02d' $H $M $S
+   fi
+}
 
 
 function testConnectivity {
@@ -45,16 +60,16 @@ while [ 1 ]
       if [ $STATUS = "active" ]; then
         if [ $online -eq 0 ]; then
           # connexion is now available
-          echo "`date "+%Y-%m-%d %H:%M:%S"` - DOWN DURING [$(($TIME-$TIMESTAMP))]" | tee -a log.csv
+          echo "`date "+%Y-%m-%d %H:%M:%S"` - DOWN DURING [$(($TIME-$TIMESTAMP))] seconds -> (`seconds2time $(($TIME-$TIMESTAMP))`)" | tee -a log.csv
           say "La connexion internet a été indisponible pendant $(($TIME-$TIMESTAMP)) secondes"
         else
           # connexion is not available
-          echo "`date "+%Y-%m-%d %H:%M:%S"` - UP DURING [$(($TIME-$TIMESTAMP))]" | tee -a log.csv
+          echo "`date "+%Y-%m-%d %H:%M:%S"` - UP DURING [$(($TIME-$TIMESTAMP))] seconds -> (`seconds2time $(($TIME-$TIMESTAMP))`)" | tee -a log.csv
           say "Attention : La connexion internet vient d'être perdue"
         fi
       fi
       TIMESTAMP=`date +%s`
     fi
-    sleep 2
+    sleep 5
     previous=$online
   done;
